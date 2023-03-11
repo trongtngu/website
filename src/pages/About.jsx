8204f033a89acc55d22019f34a27079beb1bc6ea
Trong Nguyen <trtnguyen1@gmail.com>
2023-03-11 16:47:55 +1100

    adding redirects file for netlify

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ function About () {
         Hi! I'm Trong, an aspiring Software Developer from Sydney
     </div>
     <div className="about-me-box">
-      I have programmed microcontrollers for implants and am currently working on a personal full-stack app. I enjoy playing games, going on hikes and weightlifting.
+      I have programmed microcontrollers for implants with the Mini Solar team at UNSW and am currently working on a personal full-stack app. I enjoy playing games, going on hikes and weightlifting.
     </div>
   </>)
 }
