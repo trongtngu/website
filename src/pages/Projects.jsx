d77f97c0133dc07dbd45c1f424f765e1108326ba
Trong <TrongTanNguyen424@hotmail.com>
2023-03-12 02:29:26 +1100

    projects boxes complete'

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,11 +2,34 @@ function Projects () {
   return (<>
     <div className="center-wrapper">
       <div className="projects-subtitle">projects</div>
-      <div className="projects-box">HELLO</div>
-      <div className="projects-box">HELLO</div>
-      <div className="projects-box">HELLO</div>
-      <div className="projects-box">HELLO</div>
-      <div className="projects-box">HELLO</div>
+      <div className="projects-box">
+        <div className="project-name">
+          MINI SOLAR
+        </div>
+
+        <div className="project1-img"></div>
+      </div>
+      <div className="projects-box">
+        <div className="project-name">
+          MINI SOLAR
+        </div>
+
+        <div className="project1-img"></div>
+      </div>
+      <div className="projects-box">
+        <div className="project-name">
+          MINI SOLAR
+        </div>
+
+        <div className="project1-img"></div>
+      </div>
+      <div className="projects-box">
+        <div className="project-name">
+          MINI SOLAR
+        </div>
+
+        <div className="project1-img"></div>
+      </div>
     </div>
   </>)  
 }
